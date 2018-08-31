16483248e2478d9fe3c2fbc4cc4f2c5602e116d2
Courtney Hollenkamp <cmhollenkamp@gmail.com>
2018-08-31 14:36:33 -0500

    need to set up the rainbow color

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var container = document.querySelector('.container');
-
+var divSquares = document.querySelectorAll('.squares');
 var resetButton = document.querySelector('.reset');
 var num = 16;
 
@@ -9,32 +9,57 @@ function createSquares(){
   
     container.appendChild(createDivSquares);
 }
-  
 
-var multiple = num * num;
-for (var i = 1; i <= multiple; i++){
-    createSquares();
-}
+function createGrid(num) {
+    var multiple = num * num;
+    for (var i = 1; i <= multiple; i++){
+      createSquares();
+    }
+    var heightWidth = ((512/num) - 2);
 
-
-var divSquares = document.querySelectorAll('.squares');
-var heightWidth = ((512/num) - 2);
-
-  divSquares.forEach((square) => {
+  document.querySelectorAll('.squares').forEach((square) => {
     square.style.height = `${heightWidth}`;
     square.style.width = `${heightWidth}`;
   });
+  getColor();
+}
+  
+function getColor(){
+    if (mode === true) {
+        document.querySelectorAll('.squares').forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.style.color = rainbowColor;
+          
+            });
+          });
+    } else {
+        document.querySelectorAll('.squares').forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.className += ' ' + 'colorchange';
+          
+            });
+          });
+    }
+}
 
+    createGrid(num);
 
-divSquares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-      square.className += ' ' + 'colorchange';
-
-  });
-});
-
-divSquares.forEach((square) => {
     resetButton.addEventListener('click', () => {
-        square.className = 'squares';
-  });
-});
+        container.innerHTML = ' ';
+        num = prompt('How many squares per side do you want the grid? Max 64')
+        createGrid(num);
+    });
+
+
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var rainbowColor = `rgb (${r}, ${g}, ${b})`;
+var rainbowMode;
+var rainbow = document.querySelector('.rainbow');
+rainbow.addEventListener('click', mode);
+var mode = function onRainbow() {
+    rainbow.textContent = "Rainbow On"
+    var rainbowMode = true;
+}
+console.log(mode)
